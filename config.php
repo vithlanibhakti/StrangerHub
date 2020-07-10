dbf83a294377b9e22e4f5a6dc944ae6313eec4e1
vithlanibhakti <vithlanibhakti18@gmail.com>
2020-07-10 11:24:01 +0530

    StrangerHub

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.quickdate.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | Quickdate - The Ultimate PHP Dating Platform
+// | Copyright (c) 2018 Quickdate. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$servername = "localhost";
+// MySQL Database User
+$dbusername = "root";
+// MySQL Database Password
+$dbpassword = "";
+// MySQL Database Name
+$dbname = "strangerhub";
+
+// Site URL
+$site_url = "http://localhost/Script"; // e.g (http://example.com)
+
+$app = "QuickDating";
+$endpoint_v = "v1";
+$serverkey = "87a76c9739a232f19eebfd2db323a518";
+
+// Purchase code
+$purchase_code = "03b5c7f9-9403-4a26-a055-63d297033d15"; // Your purchase code, don't give it to anyone.
+
+?>
---
+++ themes/default/index.php
@@ -0,0 +1,145 @@
+<style>
+.circ {
+	width:150px;  height:150px;
+  vertical-align: middle;
+  border-radius: 50%;
+  transition: box-shadow 0.3s;
+}
+.circ:hover {
+  box-shadow: 0 0 0 10px white;
+}
+.under {
+
+position: relative ;
+    width: 60px;
+    height: 60px;
+	left : -65px;
+	top:26px;
+	margin-left: -70px;
+	object-fit: cover;
+}
+
+.over {
+	
+  width: 200px;
+    height: 200px;
+    object-fit: cover;
+    margin: 5px 10px;
+    box-shadow: 0 2px 5px 0px rgba(0, 0, 0, 0.3);
+  
+}
+</style>
+
+		<!-- Top Hero  -->
+		<div class="parallax-container header_bg parallax_bg">
+			<div class="section">
+				<div class="container">
+					<h1 class="header center"><?php echo __( 'Meet new and interesting people.' );?></h1>
+					<div class="row center">
+						<h5 class="header col s12 light"><?php echo __( 'Join' );?> <?php echo ucfirst( $config->site_name );?>, <?php echo __( 'where you could meet anyone, anywhere!' );?></h5>
+					</div>
+				</div>
+			</div>
+			<div class="parallax"><img src="<?php echo $theme_url;?>assets/img/dating.jpg" alt="<?php echo ucfirst( $config->site_name );?>"></div>
+		</div>
+		<!-- End Top Hero  -->
+
+        <!-- Filters  -->
+		<div class="container">
+			<div class="section dt_filters bg_gradient">
+				<div class="row">
+					<div class="input-field col s2">
+						<select>
+							<option value="1" selected="selected"><?php echo __( 'Male' );?></option>
+							<option value="2"><?php echo __( 'Female' );?></option>
+						</select>
+						<label class="bold"><?php echo __( 'I am a' );?>:</label>
+					</div>
+					<div class="input-field col s2">
+						<select>
+						<option value="1"><?php echo __( 'Male' );?></option>
+							<option value="2" selected="selected"><?php echo __( 'Female' );?></option>
+						</select>
+						<label class="bold"><?php echo __( 'I\'m looking for a' );?>:</label>
+					</div>
+					<div class="input-field col s2">
+						<select>
+							<option value="18" label="18">18</option>
+							<option value="19" label="19">19</option>
+							<option value="20" label="20" selected="selected">20</option>
+							<option value="21" label="21">21</option>
+							<option value="22" label="22">22</option>
+							<option value="23" label="23">23</option>
+							<option value="24" label="24">24</option>
+							<option value="37" label="37">37</option>
+						</select>
+						<label class="bold"><?php echo __( 'Between ages' );?></label>
+					</div>
+					<div class="input-field col s2">
+						<select>
+							<option value="28" label="28">28</option>
+							<option value="29" label="29">29</option>
+							<option value="30" label="30" selected="selected">30</option>
+							<option value="31" label="31">31</option>
+							<option value="32" label="32">32</option>
+							<option value="33" label="33">33</option>
+							<option value="37" label="37">37</option>
+						</select>
+						<label class="bold"><?php echo __( 'and' );?></label>
+					</div>
+					<div class="col">
+						<a href="<?php echo $site_url;?>/login" class="waves-effect waves-light btn-flat btn-large bold btn_glossy"><?php echo __( 'Let\'s Begin' );?></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Filters  -->
+	
+
+        <?php if( $config->show_user_on_homepage == '1'){
+            $siteUsers = GetSiteUsers();
+            if( !empty($siteUsers) ){
+            ?>
+            <!-- Latest users  -->
+            <div class="container-fluid bg_gradient">
+                <div class="container">
+                    <div class="section">
+                        <div class="row">
+                            <div class="col s12 center-align dt_get_start">
+                                <h4 class="white-text light"><?php echo str_replace('{0}', ucfirst( $config->site_name ) , __( 'Latest {0} users.' ) );?></h4>
+                                <div class="center">
+                               <?php
+                                    foreach ($siteUsers as $key => $user){ ?>
+                                        <a href="<?php echo $site_url;?>/@<?php echo $user->username;?>" data-ajax="/@<?php echo $user->username;?>">
+                                            <span class="videoContainer">
+                                            <img src="<?php echo $user->mediafiles[0][avater]; ?>" alt="<?php echo $user->full_name;?>"  class="circle over circ xuser">
+                                            <img src="play-btn.png" height='100' width='100' alt="play" class="playBtn under">
+                                            </span>
+                                        </a>
+                                    <?php } ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Latest users  -->
+        <?php }} ?>
+
+
+		<br>
+		
+        		<!-- Get Started  -->
+		<div class="container-fluid bg_gradient">
+			<div class="container">
+				<div class="section">
+					<div class="row">
+						<div class="col s12 center-align dt_get_start">
+							<h4 class="white-text light"><?php echo str_replace('{0}', ucfirst( $config->site_name ) , __( 'Connect with your perfect Soulmate here, on {0}.' ) );?></h4>
+							<a href="<?php echo $site_url;?>/register" class="waves-effect waves-light btn-flat btn-large bold btn_glossy"><?php echo __( 'Get Started' );?></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Get Started  -->
