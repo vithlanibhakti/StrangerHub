2243fc0d2adb8007c07fd9e92e587c5257467837
vithlanibhakti <vithlanibhakti18@gmail.com>
2020-07-14 23:16:29 +0530

    popup

--- themes/default/partails/find-matches/matches.php
+++ themes/default/partails/find-matches/matches.php
@@ -1,3 +1,22 @@
+    <link rel="stylesheet" href= 
+"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
+    <script src= 
+"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"> 
+    </script> 
+    <script src= 
+"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"> 
+    </script> 
+    <script> 
+        $(document).ready(function() { 
+            var url = $("#Geeks3").attr('src'); 
+            $("#Geeks2").on('hide.bs.modal', function() { 
+                $("#Geeks3").attr('src', ''); 
+            }); 
+            $("#Geeks2").on('show.bs.modal', function() { 
+                $("#Geeks3").attr('src', url); 
+            }); 
+        }); 
+    </script> 
 <?php global $site_url; ?>
 <div class="mtc_usrd_content" data-id="<?php echo $matche->id;?>" <?php if($matche_first === false){?> style="display: none;"<?php }?>>
     <div class="row">
@@ -97,35 +116,65 @@
                         </div>
                     <?php 
 					}
-					
+				
 					if($matche->smoke){?>
                         <div>
                             <p class="info_title"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M2,16H17V19H2V16M20.5,16H22V19H20.5V16M18,16H19.5V19H18V16M18.85,7.73C19.47,7.12 19.85,6.28 19.85,5.35C19.85,3.5 18.35,2 16.5,2V3.5C17.5,3.5 18.35,4.33 18.35,5.35C18.35,6.37 17.5,7.2 16.5,7.2V8.7C18.74,8.7 20.5,10.53 20.5,12.77V15H22V12.76C22,10.54 20.72,8.62 18.85,7.73M16.03,10.2H14.5C13.5,10.2 12.65,9.22 12.65,8.2C12.65,7.18 13.5,6.45 14.5,6.45V4.95C12.65,4.95 11.15,6.45 11.15,8.3A3.35,3.35 0 0,0 14.5,11.65H16.03C17.08,11.65 18,12.39 18,13.7V15H19.5V13.36C19.5,11.55 17.9,10.2 16.03,10.2Z"></path></svg> <?php echo __('Smoke');?></p>
                             <span><?php echo $matche->smoke;?></span>
                         </div>
                     <?php }
-
 					if($matche->ethnicity){?>
                         <div>
                             <p class="info_title"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M5,9.5L7.5,14H2.5L5,9.5M3,4H7V8H3V4M5,20A2,2 0 0,0 7,18A2,2 0 0,0 5,16A2,2 0 0,0 3,18A2,2 0 0,0 5,20M9,5V7H21V5H9M9,19H21V17H9V19M9,13H21V11H9V13Z"></path></svg> <?php echo __('Ethnicity');?></p>
                             <span><?php echo $matche->ethnicity;?></span>
                         </div>
-                    <?php }
-			
-			$name=$matche->username;
-			?> 
-
-				
+     <?php }		
+	$name=$matche->username;
+    include("config.php");
+	$sql=mysqli_query($con,"SELECT id from users where username='$name'");
+	while($row=mysqli_fetch_assoc($sql))
+		{
+			foreach($row as $id)
+			{
+				//echo "<br>".$id;
+			}
+		}
+		$fetchVideos = mysqli_query($con, "SELECT * FROM videos where id='$id' ORDER BY id DESC");
+        while($row = mysqli_fetch_assoc($fetchVideos)){
+            $location = $row['location'];          
+			//echo $location;
+           // echo "<video src='".$location."' id='Geeks3' controls width='320px' height='200px' >";
+        }
+		?> 
 	<div class="vew_profile">
-   
-   
+   <!--<a href='readvideos.php?username=<?php echo $name ?>' class="btn waves-effect">Video</a>
 
-    <a href='readvideos.php?username=<?php echo $name ?>' class="btn waves-effect">Video</a>
-	<!--
    <a href="<?php echo $site_url;?>/@<?php echo $matche->username;?>"
    data-ajax="/@<?php echo $matche->username;?>" class="btn waves-effect">Video</a>
    -->
-	
+	<div class="bs-example"> 
+            <a href="#Geeks2"
+            class="btn btn-lg btn-primary waves-effect" data-toggle="modal">View Video</a> 
+  
+            <div id="Geeks2" class="modal fade"> 
+                <div class="modal-dialog"> 
+                    <div class="modal-content"> 
+                        <div class="modal-header"> 
+						<div align="left">
+                            <button type="button" data-dismiss="modal" aria-hidden="true"> ×</button> 
+                        </div> 
+                        </div> 
+                        <div class="modal-body"> 
+            <!--<iframe id="Geeks3" width="450" height="350" src='".$location."' controls width='320px' height='200px'
+                                    frameborder="0" allowfullscreen> -->
+									<video src='<?php echo $location; ?>' id='Geeks3' controls width='320px' height='200px' >
+									
+                            </iframe> 
+                        </div> 
+                    </div> 
+                </div> 
+        </div> 
+    </div> 
                 </div>
 			<?php
 		}
