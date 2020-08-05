5f627433a927515d1dc5e39b863d7452f2c1f850
vithlanibhakti <vithlanibhakti18@gmail.com>
2020-08-05 19:45:20 +0530

    vcpremium

--- themes/default/main/chat.php
+++ themes/default/main/chat.php
@@ -175,10 +175,26 @@
                             ?>
 
                             <?php if ($video_link == true) { 
+                                   $id= $profile->id;
+                                		include("config.php");
+									
+										$sql  = mysqli_query($con, "SELECT `id`, `user_id`, `amount`, `type`, `date`, `pro_plan`, `credit_amount`, `via`, `pro_video` FROM `vcpayments` WHERE user_id=$id");
+										 	while($row = mysqli_fetch_assoc($sql)){
+										 		$user_id = $row['user_id'];
+                                                }
+                                        if($user_id >= '1')
+                                        {
+                                            ?>
+
+                                            <a href="javascript:void(0);" onclick="Wo_GenerateVideoCall(<?php echo auth()->id;?>)"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M17,10.5V7A1,1 0 0,0 16,6H4A1,1 0 0,0 3,7V17A1,1 0 0,0 4,18H16A1,1 0 0,0 17,17V13.5L21,17.5V6.5L17,10.5Z"></path></svg> <?php echo __('Video call');?></a>               
+                                       <?php }
+                                       else{
+				                   
                                    ?>
+                                   
                         <!-- <a href="javascript:void(0);" onclick="Wo_GenerateVideoCall(<?php echo auth()->id;?>)"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M17,10.5V7A1,1 0 0,0 16,6H4A1,1 0 0,0 3,7V17A1,1 0 0,0 4,18H16A1,1 0 0,0 17,17V13.5L21,17.5V6.5L17,10.5Z"></path></svg> <?php echo __('Video call');?></a> -->
                         <a href="<?php echo $site_url;?>/vcpro" data-ajax="/vcpro"><span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M17,10.5V7A1,1 0 0,0 16,6H4A1,1 0 0,0 3,7V17A1,1 0 0,0 4,18H16A1,1 0 0,0 17,17V13.5L21,17.5V6.5L17,10.5Z"></path></svg></span><?php echo __( 'Video call' );?></a>
-                            <?php } ?>
+                            <?php } } ?>
                             <?php if ($audio_link == true) { ?>
                                 <a href="javascript:void(0);" onclick="Wo_GenerateVoiceCall(<?php echo auth()->id;?>)"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"></path></svg> <?php echo __('Audio call');?></a>
                             <?php } ?>
